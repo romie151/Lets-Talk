b6763a495d6e36536195720310a192da7587a8aa
romie151 <romie151@yahoo.com>
2018-06-08 16:17:39 -0400

    created the login and registration pages

--- react-frontend/src/App.js
+++ react-frontend/src/App.js
@@ -1,19 +1,15 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
-
+import Home from './Components/Home';
+import Register from './Components/Register'
+import { Link, Route, Switch } from 'react-router-dom'
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+        <Switch>
+          <Route exact path='/home' component={Home} />
+          <Route path='/register' component={Register}/>
+        </Switch>
     );
   }
 }
